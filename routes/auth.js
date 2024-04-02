01304425520a560559c0209d10cca21930ba966a
safwansadi <safwaniftekher@gmail.com>
2024-04-02 05:13:49 -0700

    refactor

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const Joi = require("@hapi/joi");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const _ = require("lodash");
 const { User } = require("../models/user");
 const mongoose = require("mongoose");
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const auth = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
